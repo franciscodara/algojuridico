f1e7d22723a7baba2eac6b71fb758901f7836e96
franciscodara <franciscodara@gmail.com>
2021-07-27 15:07:56 -0300

    base limpa

---
+++ src/wp-content/themes/codara-legal-theme/front-page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Algo Jurídico</title>
+</head>
+<body>
+    <h1>Title 3</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum non quos expedita corporis officia voluptates veniam incidunt eos sit laboriosam autem, voluptas pariatur odio tempora asperiores voluptatum dolore omnis quo.</p>
+</body>
+</html>
